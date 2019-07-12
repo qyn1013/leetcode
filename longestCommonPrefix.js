9f31faa2070503933194598427452a3b05a884cf
qyn1013 <877196019@qq.com>
2019-07-12 16:30:42 +0800

    fext(code): longestCommonPrefix leetcode 14

---
+++ longestCommonPrefix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    var firstStrs = strs[0];
+    var result ='';
+    if(!strs.length){
+        return result;
+    }
+    for (var i = 0; i < firstStrs.length; i++) {
+        for (var j =  1; j < strs.length; j++) {
+            if(firstStrs[i] !== strs[j][i]){
+                return result;
+            }
+         }
+         result += firstStrs[i];
+     }
+     return result;
+  };
